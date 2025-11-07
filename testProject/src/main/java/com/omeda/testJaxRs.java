805e52ba82653dcc678bf3e3ccf4e764c97babb0
smahadevan <smahadevan@omeda.com>
2025-11-07 10:03:44 -0600

    test

--- testProject/src/main/java/com/omeda/testJaxRs.java
+++ testProject/src/main/java/com/omeda/testJaxRs.java
@@ -1,15 +1,7 @@
 package com.omeda;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-
 public class testJaxRs {
-    @GET
-    @Path("{/upload-file/internal/source-number/{sourceNumber}")
-    @Produces({MediaType.APPLICATION_JSON})
-    @Operation(summary = "Get file data from an internal audience source.",
-            description="Get file data details for a single internal audience source.")
-    public void getInternalSourceFileDetails() {
-        System.out.println("test jax rs");
-    }
 
+    public void data() {
+        System.out.println("testJaxRs data");
+    }
 }
