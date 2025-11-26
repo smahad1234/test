e6d0775bb78070156842b753a3600a07b26ed53a
smahadevan <smahadevan@omeda.com>
2025-11-26 11:40:43 -0600

    made changes

---
+++ test-ai/src/main/java/com/omeda/testJaxRs.java
@@ -0,0 +1,20 @@
+package com.omeda;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+public class testJaxRs {
+
+    @GET
+    @Path("{" + OdysseyConstants.ELEMENT_ID + "}/test")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed(PermissionConstants.ODY_APPLICATION_ACCESS_NEW_UI)
+    @Operation(summary = "Load Audience Element.", description = "Load Audience Element data.")
+    public AudienceElementSchema getAudience(@BeanParam RequestedDatabase requestDetail,
+                                             @NotNull @PathParam(OdysseyConstants.ELEMENT_ID) Integer elementId) throws ParseException {
+
+        System.out.println("testJaxRs getAudience called with elementId: " + elementId);
+        return new AudienceElementSchema();
+
+    }
+
+}
